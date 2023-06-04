b09713768a2f43190cc94bafcd7c2f1a75c7e5fd
nickash2 <nickash015@gmail.com>
2023-06-04 02:07:58 +0200

    Added even, odd, div2 functions

--- arith.pl
+++ arith.pl
@@ -23,6 +23,18 @@ times(X,s(Y),Z) :- times(X,Y,Z1), plus(X,Z1,Z).
 pow(X,0,s(0)) :- isnumber(X).
 pow(X,s(Y),Z) :- pow(X,Y,Z1), times(X,Z1,Z).
 
+% even(X) is true if N is even
+
+even(X) :- mod(X,2) =:= 0.
+
+% odd(X) is true if N is odd
+
+odd(X) :- mod(X,2) =\= 0.
+
+
+div2(N, D) :- plus(D, D, N).
+
+
 % Example queries:
 % Isnumbers are represented as successors of 0. For example, 2 is s(s(0)).
 % 2+2=4 is plus(s(s(0)), s(s(0)), s(s(s(s(0)))))
